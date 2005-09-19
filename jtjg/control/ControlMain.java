bacbb3ab48093010eebb20f5dd9eee6cffaa1bac
ralix <ralix>
2005-09-19 16:16:45 +0000

    change version-String to 0.2.9c

--- jtjg/control/ControlMain.java
+++ jtjg/control/ControlMain.java
@@ -78,7 +78,7 @@ public class ControlMain {
 
 	private static String settingsFilename;
 
-	public static String version[] = {"XMediaGrabber 0.2.9b", "06.02.2005", "User: " + System.getProperty("user.name")};
+	public static String version[] = {"XMediaGrabber 0.2.9c", "06.02.2005", "User: " + System.getProperty("user.name")};
 
 	public static void main(String args[]) {
 	    startSplash();
